0f26d7715ccabdfa56dfdf6879294bcd952d1afd
Brooke Watson <watson@ecohealthalliance.org>
2016-09-20 17:58:35 -0400

    Small edits

--- EIDR.R
+++ EIDR.R
@@ -13,27 +13,25 @@ setwd(wd)
 eidr <- read.csv(file = "eidr.csv", header = TRUE, stringsAsFactors = FALSE)
 dim(eidr)
 names(eidr)
+str(eidr)
 
 #selecting variables of interest
 eidr2 <- dplyr::select(eidr, Event.Name:Start.Date, Number.Infected, Number.of.Deaths,
                        Zoonotic..Not.Event.Specific.,
         Average.Age.of.Infected:Life.Expectancy.in.Country.in.the.first.Year.of.Event)
 
-#converting factors to integers
+#tidying numeric data 
 for (i in c(9,10,12)){
   eidr2[,i] <- gsub("years", "", eidr2[,i], perl=TRUE)
 }
-
- 
 
 #adding NAs  
 for (i in 1:12){
   eidr2[,i] <- gsub("Not Applicable", "NA", eidr2[,i], perl=TRUE)
   eidr2[,i] <- gsub("Not Found", "NA", eidr2[,i], perl=TRUE)
 }
+ 
 
-
-#making infection and death rates numeric 
 for (i in c(6:7, 12)){
   eidr2[,i] <- as.numeric(as.character(eidr2[,i]))
 } 
@@ -43,7 +41,7 @@ eidr2 <- mutate(eidr2, Fatality.Rate = Number.of.Deaths/Number.Infected)
 
 #summing missing values  
 missing.infected <- sum(is.na(eidr2[,6])) # 44 missing values for number of infected 
-missing.deaths <- sum(is.na(eidr2[,7])) # 107 missing values for number of fatalities 
+missing.deaths <- sum(is.na(eidr2[,7])) # 107  
 missing.fatality.rate <- sum(is.na(eidr2[,13])) #113 
 
 ##central tendancy values for number of infected and number of deaths 
@@ -52,19 +50,34 @@ meandeath <- sum(eidr2$Number.of.Deaths, na.rm = TRUE)/nrow(eidr2) #0
 medinfxd <- median(eidr2$Number.Infected, na.rm = TRUE) #10295.2 
 meddeath <- median(eidr2$Number.of.Deaths, na.rm = TRUE) # 62.4
 
-#arranged, ordered sequence variables for plotting one-dimensional histograms  
+#arranged, ordered sequence variables for plotting one-dimensional histograms 
 eidr.i <- arrange(eidr2, Number.Infected)
 I.Count <- seq_along(eidr.i$Number.Infected)  
 
 eidr.d <- arrange(eidr2, Number.of.Deaths)
 D.Count <- seq_along(eidr.d$Number.of.Deaths) #another sequence variable 
- 
 
- 
-#plotting 
-gi <- ggplot(eidr.i, aes(I.Count, Number.Infected))+geom_point()
-gd <- ggplot(eidr.d, aes(D.Count, Number.of.Deaths))+geom_point()
-grid.arrange(gi, gd, nrow=2, top = "histograms of infection and death frequencies in EIDs")
+# #plotting 
+# gi <- ggplot(eidr.i, aes(I.Count, Number.Infected))+geom_point()
+# gd <- ggplot(eidr.d, aes(D.Count, Number.of.Deaths))+geom_point()
+# grid.arrange(gi, gd, nrow=2, top = "relative impact of EIDs")
+
+
+#frequency histogram (bar plot)
+ggplot(eidr.d, aes(Number.of.Deaths)) +
+  geom_histogram()
+
+#colored by zoonosis status 
+ggplot(eidr.d, aes(Number.of.Deaths, fill=Zoonotic..Not.Event.Specific.)) +
+  geom_histogram()
+
+#as a line
+ggplot(eidr.d, aes(Number.of.Deaths)) +
+  geom_freqpoly()
+
+#lines
+ggplot(eidr.d, aes(Number.of.Deaths, color=Zoonotic..Not.Event.Specific.)) +
+  geom_freqpoly(bins=50)
 
 #just deaths 
 gd <- ggplot(eidr.d, aes(D.Count, Number.of.Deaths))
